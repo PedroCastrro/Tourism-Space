2d2fd24ddaebbc08810f9f166246563e0e8ed2f9
Pedro Castro <pgkastro@gmail.com>
2022-10-27 08:48:55 -0300

    background image-page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import './Header.modules.css';
 
 export default function Header() {
     return (
        <div>
         <header>
+            <img src="logo.svg" alt="" />
             <nav>
                 <ul>
                     <Link to="/">00 Home</Link>
