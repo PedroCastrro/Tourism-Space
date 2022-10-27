b89edca0ebaf243aa6c96f69f1bb7a4e31f01f2d
Pedro Castro <pgkastro@gmail.com>
2022-10-27 18:47:55 -0300

    Home finish

--- src/Styles/Styles.js
+++ src/Styles/Styles.js
@@ -4,23 +4,44 @@ export const Body = styled.body`
     display: flex;
 `;
 
+export const Main = styled.main`
+    display: block;
+    margin-top: 30vh;
+    margin-left: 20vh;
+`;
+
 export const Aside = styled.aside`
+    display: inline-block;
     background-color: #fff;
+    position: relative;
+    margin-top: 48vh;
+    margin-left: 60vh;
+    width: 25vh;
+    height: 25vh;
+    border-radius: 50%;
 `;
 
 export const Title = styled.h3`
     color: #fff;
+    font-size: 30px;
 `;
 
 export const Title1 = styled.h2`
   color: #fff;  
+  font-size: 100px;
+  margin-top: 1vh;
 `;
 
 export const Paragrafo = styled.p`
     color: #fff;
+    text-align: left;
+    margin-top: 2vh;
 `;
 
 export const Title3 = styled.h2`
     color: #423E3D;
+    text-align: center;
+    margin-top: 10vh;
+    font-size: 30px;
    
 `;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,8 @@ import React from "react";
 import Header from '../Components/Header';
 import background from "../imagens/background-home-desktop.jpg";
 
-import { Title, Title1, Paragrafo, Title3, Body, Aside} from '../Styles/Styles';
-import { Component } from 'react';
+import { Title, Title1, Paragrafo, Title3, Body, Aside, Main} from '../Styles/Styles';
+
 
 export default function Home() {
   return (
@@ -15,16 +15,16 @@ export default function Home() {
         backgroundRepeat: 'no-repeat' } }>
         <Header />
       <Body>
-        <main>
-          <Title> So, you want to travel to</Title>
+        <Main>
+          <Title> so, you want to travel to</Title>
           <Title1>Space</Title1>
           <Paragrafo>
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience! Explore{' '}
+            Let’s face it; if you want to go to space, you might as well<br/>
+            genuinely go to outer space and not hover kind of on the<br />
+            edge of it. Well sit back, and relax because we’ll give you<br />
+            a truly out of this world experience! Explore{' '}
           </Paragrafo>
-        </main>
+        </Main>
 
         <Aside>
           <Title3>Explorer</Title3>
