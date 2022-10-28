42c57591f440ac93d4b81073ff43195953f30bbd
Pedro Castro <pgkastro@gmail.com>
2022-10-28 09:09:42 -0300

    initialize Crew page

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import Header from '../Components/Header';
-import background from "../imagens/background-crew-desktop.jpg";
+import React from 'react'
+import Header from '../Components/Header'
+import Ansari from '../imagens/image-anousheh-ansari.png'
+import background from '../imagens/background-crew-desktop.jpg'
+
+import {} from '../Styles/Styles'
 
 export default function Crew() {
-    return (
-        <div style={{ backgroundImage: `url(${background})`,
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${background})`,
         position: 'relative',
         height: '100vh',
         width: '100vw',
         backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat' }}>
-            <Header />
-            <h1>Crew</h1>
-        </div>
-    );
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <Header />
+      <body>
+        <main>
+          <h2>02 Meet your crew</h2>
+          <h3>Flight Engineer</h3>
+          <h3>Anousheh Ansari</h3>
+          <p>
+            Anousheh Ansari is an Iranian American engineer and co-
+            <br />
+            founder of Prodea Systems. Ansari was the fourth self-
+            <br />
+            funded space tourist, the first self-funded woman to fly
+            <br />
+            to the ISS, and the first Iranian in space.
+          </p>
+          <section>
+
+          </section>
+        </main>
+        <aside>
+            <img src={Ansari} alt="" />
+        </aside>
+      </body>
+    </div>
+  )
 }
